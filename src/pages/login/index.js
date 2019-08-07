ecaa39a792e6656fc55e0b16a3c3e4c3f83f79ba
jianping888 <229533511@qq.com>
2019-08-07 15:45:20 +0800

    多页面打包

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import "./index.scss";
+import "../../assets/css/commen.scss";
+var add=()=>{
+    console.log("this",this)
+}
+add()
+
+function ele(){
+    console.log("this222",this)
+
+}
+ele()
